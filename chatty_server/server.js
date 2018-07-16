47a93a0417d8ff1623b8231689a1b75ed9462ffe
Farid <faridbardium@gmail.com>
2018-07-16 00:13:18 +0000

    sending messages to the client from the server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,16 +1,18 @@
 // server.js
-
-const express = require('express');
-const SocketServer = require('ws').Server;
+const express = require("express");
+const SocketServer= require("ws").Server;
+const uuid = require("uuid/v4");
 
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static("public"))
+  .listen(PORT, "0.0.0.0", "localhost", () =>
+    console.log(`Listening on ${PORT}`)
+  );
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
@@ -18,14 +20,26 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  // onmessage
-  //recieve messages
-  ws.on('message', function incoming(data) {
-    console.log(JSON.parse(data));
+wss.on("connection", (ws, req) => {
+
+  console.log("Client connected");
+  
+  //receive messages
+  ws.on("message", data => {
+
+    let newMessage = JSON.parse(data)
+    newMessage.id = uuid()
+    console.log(newMessage)
+
+    if (newMessage.type === "message") {
+        // socketServer.
+        wss.clients.forEach(user => {
+          user.send(JSON.stringify(newMessage));
+        });
+    }
+
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on("close", () => console.log("Client disconnected"));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -16,30 +16,11 @@ class App extends Component {
   componentDidMount() {
     //server
     this.connectSocket = new WebSocket("ws://0.0.0.0:3001");
-    
-    this.connectSocket.onopen = () => {
-      console.log("sending message");
-      this.connectSocket.send("Testing message from App");
-      // this.connectSocket.send(JSON.stringify(msgContent))
 
-    };
-
-    //    this.connectSocket.onsomething = function() {
-    
-
-    // setTimeout(() => {
-    //   console.log("Simulating incoming message");
-    //   // Add a new message to the list of messages in the data store
-    //   const newMessage = {
-    //     id: 3,
-    //     username: "Michelle",
-    //     content: "Hello there!"
-    //   };
-    //   const messages = this.state.messages.concat(newMessage);
-    //   // Update the state of the app component.
-    //   // Calling setState will trigger a call to render() in App and all child components.
-    //   this.setState({ messages: messages });
-    // }, 3000);
+    this.connectSocket.onmessage = (e) => {
+      let parsedMessage = JSON.parse(e.data)
+      console.log(parsedMessage)
+    }
   }
 
   sendUsername = usrContent => {
@@ -53,23 +34,23 @@ class App extends Component {
   // need to define this in app to update the state
   sendMessage = msgContent => {
     const messages = [...this.state.messages];
-    
-    const messageObj = {
-      username: this.state.currentUser.name,
-      content: msgContent
 
-    }
+    const messageObj = {
+      type: "message",
+      data: {
+        username: this.state.currentUser.name,
+        content: msgContent
+      }
+    };
     // messages.push({
     // });
-    
+
     this.setState({
       messages: messages
     });
 
-    this.connectSocket.send(JSON.stringify(messageObj))
-    console.log(msgContent)
-    
-    
+    this.connectSocket.send(JSON.stringify(messageObj));
+    console.log(msgContent);
   };
 
   render() {
@@ -85,5 +66,6 @@ class App extends Component {
     );
   }
 }
+
 export default App;
 // onKeyPress = {}/>
